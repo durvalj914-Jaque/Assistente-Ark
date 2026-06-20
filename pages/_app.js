210e7a93368c41445f10555736f5efb0cde4d188
durvalj914-Jaque <durvalj914@gmail.com>
2026-06-20 08:18:31 -0300

    fix: corrigir sintaxe do link favicon que quebrava o build

--- pages/_app.js
+++ pages/_app.js
@@ -33,7 +33,7 @@ export default function App({ Component, pageProps }) {
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="theme-color" content="#080810" />
         <link rel="canonical" href={canonicalUrl} />
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🤖</text></svg>" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
     </>
